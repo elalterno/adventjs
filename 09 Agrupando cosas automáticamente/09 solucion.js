6a2b6ef6b6860237c4a47e34ca834b71571fe410
José Raúl Beltran Orjuela <jbeltran@veritran.com>
2022-03-03 10:32:49 -0500

    solucion 09

---
+++ 09 Agrupando cosas automáticamente/09 solucion.js
@@ -0,0 +1,24 @@
+export default function groupBy(collection, it) {
+	if (typeof (it) === 'function')
+		return collection.reduce((result, item) => {
+		const key = it(item);
+		if (result[key]){
+			result[key].push(item);
+		}
+		else{ 
+			result[key] = [item];
+		}
+		return result;
+	}, {});
+
+	return collection.reduce((result, item) => {
+	const key = item[it];
+	if (result[key]){
+		result[key].push(item);
+	}
+	else{
+		result[key] = [item];
+	}
+	return result;
+	}, {});
+}
